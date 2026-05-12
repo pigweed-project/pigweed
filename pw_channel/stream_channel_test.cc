4a8aa71e505dea99356aed9c7ebd639d0a8fef3c
Wyatt Hepler <hepler@google.com>
2026-05-12 12:37:51 -0700

    pw_stream: Delete MpscStream
    Delete MpscStream, which is unused outside of Pigweed. Its test was
    occasionally failing with TSAN flakes.
    
    Change-Id: I4603f13c3d57015d0ef3f621384d3d28897b1b45
    Reviewed-on: https://pigweed-review.googlesource.com/c/pigweed/pigweed/+/408212

--- pw_channel/stream_channel_test.cc
+++ pw_channel/stream_channel_test.cc
@@ -16,6 +16,7 @@
 
 #include <algorithm>
 #include <array>
+#include <mutex>
 
 #include "pw_async2/dispatcher_for_test.h"
 #include "pw_async2/func_task.h"
@@ -24,7 +25,8 @@
 #include "pw_memory/no_destructor.h"
 #include "pw_multibuf/simple_allocator_for_test.h"
 #include "pw_status/status.h"
-#include "pw_stream/mpsc_stream.h"
+#include "pw_sync/mutex.h"
+#include "pw_sync/thread_notification.h"
 #include "pw_thread/test_thread_context.h"
 #include "pw_unit_test/framework.h"
 
@@ -38,6 +40,79 @@ using ::pw::async2::Ready;
 using ::pw::multibuf::MultiBuf;
 using ::pw::multibuf::test::SimpleAllocatorForTest;
 using ::pw::operator""_b;
+
+class SimplePipe {
+ public:
+  class PipeReader final : public pw::stream::NonSeekableReader {
+   public:
+    PipeReader(SimplePipe& pipe) : pipe_(pipe) {}
+
+   private:
+    pw::StatusWithSize DoRead(pw::ByteSpan dest) override {
+      return pipe_.Read(dest);
+    }
+    SimplePipe& pipe_;
+  };
+
+  class PipeWriter final : public pw::stream::NonSeekableWriter {
+   public:
+    PipeWriter(SimplePipe& pipe) : pipe_(pipe) {}
+
+   private:
+    pw::Status DoWrite(pw::ConstByteSpan data) override {
+      return pipe_.Write(data);
+    }
+    SimplePipe& pipe_;
+  };
+
+  SimplePipe() : reader_(*this), writer_(*this) {}
+
+  PipeReader& GetReader() { return reader_; }
+  PipeWriter& GetWriter() { return writer_; }
+
+ private:
+  pw::StatusWithSize Read(pw::ByteSpan dest) {
+    while (true) {
+      {
+        std::lock_guard lock(mutex_);
+        if (size_ > 0) {
+          size_t to_read = std::min(dest.size(), size_);
+          for (size_t i = 0; i < to_read; ++i) {
+            dest[i] = buffer_[tail_];
+            tail_ = (tail_ + 1) % buffer_.size();
+          }
+          size_ -= to_read;
+          static_cast<void>(data_available_.try_acquire());
+          return pw::StatusWithSize(to_read);
+        }
+      }
+      data_available_.acquire();
+    }
+  }
+
+  pw::Status Write(pw::ConstByteSpan data) {
+    std::lock_guard lock(mutex_);
+    if (size_ + data.size() > buffer_.size()) {
+      return pw::Status::ResourceExhausted();
+    }
+    for (std::byte b : data) {
+      buffer_[head_] = b;
+      head_ = (head_ + 1) % buffer_.size();
+    }
+    size_ += data.size();
+    data_available_.release();
+    return pw::OkStatus();
+  }
+
+  PipeReader reader_;
+  PipeWriter writer_;
+  pw::sync::Mutex mutex_;
+  pw::sync::ThreadNotification data_available_;
+  std::array<std::byte, 16> buffer_{};
+  size_t head_ = 0;
+  size_t tail_ = 0;
+  size_t size_ = 0;
+};
 
 template <typename ActualIterable, typename ExpectedIterable>
 void ExpectElementsEqual(const ActualIterable& actual,
@@ -58,14 +133,8 @@ void ExpectElementsEqual(const ActualIterable& actual,
 }
 
 struct LiveForeverTestData {
-  LiveForeverTestData() {
-    pw::stream::CreateMpscStream(channel_input_reader, channel_input_writer);
-    pw::stream::CreateMpscStream(channel_output_reader, channel_output_writer);
-  }
-  pw::stream::BufferedMpscReader<512> channel_input_reader;
-  pw::stream::MpscWriter channel_input_writer;
-  pw::stream::BufferedMpscReader<512> channel_output_reader;
-  pw::stream::MpscWriter channel_output_writer;
+  SimplePipe channel_input_pipe;
+  SimplePipe channel_output_pipe;
   SimpleAllocatorForTest<> allocator;
   pw::thread::test::TestThreadContext read_thread_cx;
   pw::thread::test::TestThreadContext write_thread_cx;
@@ -75,9 +144,9 @@ TEST(StreamChannel, ReadsAndWritesData) {
   static pw::NoDestructor<LiveForeverTestData> test_data;
   static pw::RuntimeInitGlobal<pw::channel::StreamChannel> stream_channel(
       test_data->allocator,
-      test_data->channel_input_reader,
+      test_data->channel_input_pipe.GetReader(),
       test_data->read_thread_cx.options(),
-      test_data->channel_output_writer,
+      test_data->channel_output_pipe.GetWriter(),
       test_data->write_thread_cx.options());
 
   FuncTask read_task([&](Context& cx) -> Poll<> {
@@ -109,7 +178,7 @@ TEST(StreamChannel, ReadsAndWritesData) {
   EXPECT_TRUE(dispatcher.RunUntilStalled());
   std::array<const std::byte, 3> data_to_send({1_b, 2_b, 3_b});
   ASSERT_EQ(pw::OkStatus(),
-            test_data->channel_input_writer.Write(data_to_send));
+            test_data->channel_input_pipe.GetWriter().Write(data_to_send));
   dispatcher.RunToCompletion();
 }
 
