850040e31f99084aa256054a137d1dacf820790b
Lloyd Pique <lpique@google.com>
2026-07-13 12:20:53 -0700

    pw_async2: Reduce code size for coroutine allocation
    http://pwrev.dev/42995 fixed a bug encountered with a newer LLVM
    toolchain. However it was was written with a bias towards genericity
    reusing BumpAllocator from pw_allocator, as well as internally using a
    "FramedAllocation" templated helper that might have been refactored out
    to be shared with other allocation code.
    
    The overhead of all of that resulted in a code size increase of about
    300 bytes, as shown at  https://pigweed.dev/pw_async2/code_size.html.
    
    See the row labeled: "Size of pw_async2’s C++ coroutine adapters".
    
    That extra cost was noticed. This change drastically reduces it:
    
    - Remove the use of BumpAllocator, replacing it with simpler local
      pointer adjustments. This saved 272 bytes, as BumpAllocator uses a
      non-inlineable virtual interface, along with other overhead.
    - Remove the templated "FramedAllocation" helper class. Despite it
      being instantiated for only one type, with inlining possible,
      eliminating it in favor of simpler pointer management saved another
      64 bytes.
    - Remove the call to resize the allocation based on the space actually
      used. This saves another 16 bytes, and its likely there wouldn't be
      much space returned to the allocator anyway.
    - Store the pointer to the outer allocation, rather than an offset. This
      saves an additional 16 bytes by eliminating the need for some pointer
      arithmetic.
    - Removing the added PW_CHECK/PW_DCHECK calls. The original
      implementation didn't have similar checks. This saved another 80 bytes
      according to the size report.
    
    Here are the results from the pw_async2/code_size report:
    
    - Before the LLVM bugfix: +1,520
    - After the LLVM bugfix: +1,808
    - After these optimizations: +1,360
    
    In other words, a net savings of 160 bytes over the pre-bugfix code.
    
    Test: bazelisk test //pw_async2/...
    Test: Examine pw_async2/code_size.html
    Change-Id: Id5ec409e731cbc9b7fd45e749831fda0ca4e0fd0
    Reviewed-on: https://pigweed-review.googlesource.com/c/pigweed/pigweed/+/433693

--- pw_async2/coro.cc
+++ pw_async2/coro.cc
@@ -22,141 +22,26 @@
 #include <tuple>
 #include <utility>
 
-#include "pw_allocator/bump_allocator.h"
 #include "pw_allocator/deallocator.h"
 #include "pw_allocator/layout.h"
-#include "pw_assert/check.h"
 #include "pw_async2/coro.h"
 #include "pw_bytes/alignment.h"
-#include "pw_bytes/span.h"
 #include "pw_log/log.h"
 
 namespace pw::async2::internal {
 namespace {
 
-// TODO: https://pwbug.dev/529845745 - Extract utility functions to use here and
-// with FramingAllocator to reduce similar/duplicate code.
-
-template <typename Prefix>
-class FramedAllocation {
- public:
-  static allocator::Layout GetOuterLayout(allocator::Layout inner_layout);
-  template <typename... Args>
-  static std::pair<Prefix*, void*> Allocate(Allocator* allocator,
-                                            allocator::Layout inner_layout,
-                                            Args&&... args);
-  static Prefix& GetPrefixFromInner(void* inner);
-  static void DeallocateFromInner(Allocator* allocator, void* inner);
-
- private:
-  struct Header {
-    Prefix prefix;
-    size_t offset;
-
-    template <typename... Args>
-    explicit Header(size_t offs, Args&&... args)
-        : prefix(std::forward<Args>(args)...), offset(offs) {}
-  };
-
-  static Header& GetHeaderFromInner(void* inner);
-
-  static_assert(alignof(Header) <= alignof(size_t),
-                "alignof(Header) cannot exceed alignof(size_t)");
-
-  static constexpr allocator::Layout kHeaderLayout =
-      allocator::Layout::Of<Header>();
-};
-
-template <typename Prefix>
-allocator::Layout FramedAllocation<Prefix>::GetOuterLayout(
-    allocator::Layout inner_layout) {
-  // Note: This is intentionally an overestimate of the size needed.
-  // We need enough extra room to:
-  //   - A prefix (offset + pointer)
-  //   - Padding to realign to the alignment needed for the coroutine state
-  const size_t padding_size = inner_layout.alignment();
-  return {inner_layout.size() + kHeaderLayout.size() + padding_size,
-          kHeaderLayout.alignment()};
-}
-
-template <typename Prefix>
-template <typename... Args>
-std::pair<Prefix*, void*> FramedAllocation<Prefix>::Allocate(
-    Allocator* allocator, allocator::Layout inner_layout, Args&&... args) {
-  PW_DCHECK(allocator != nullptr);
-
-  const auto outer_layout = GetOuterLayout(inner_layout);
-
-  auto* outer_alloc = allocator->Allocate(outer_layout);
-  if (outer_alloc == nullptr) {
-    return {nullptr, nullptr};
-  }
-
-  const pw::ByteSpan bytes = {static_cast<std::byte*>(outer_alloc),
-                              outer_layout.size()};
-  allocator::BumpAllocator bump_allocator(bytes);
-
-  // Reserve space for the prefix. Note the prefix will actually be located
-  // after any padding needed to align the inner allocation, such that the
-  // prefix immediately precedes the inner allocation in memory.
-  std::ignore = bump_allocator.Allocate(kHeaderLayout);
-
-  // Allocate space for the inner/wrapped allocation.
-  void* const inner_alloc = bump_allocator.Allocate(inner_layout);
-
-  // Construct the header (including prefix) in the space between the outer
-  // allocation and the inner
-  Header* const header = std::construct_at<Header>(
-      reinterpret_cast<Header*>(reinterpret_cast<uintptr_t>(inner_alloc) -
-                                sizeof(Header)),
-      reinterpret_cast<uintptr_t>(inner_alloc) -
-          reinterpret_cast<uintptr_t>(outer_alloc),
-      std::forward<Args>(args)...);
-
-  // Trim and return any excess memory.
-  const size_t used = outer_layout.size() - bump_allocator.remaining();
-  std::ignore = allocator->Resize(outer_alloc, used);
-
-  return {&header->prefix, inner_alloc};
-}
-
-template <typename Prefix>
-FramedAllocation<Prefix>::Header& FramedAllocation<Prefix>::GetHeaderFromInner(
-    void* inner_alloc) {
-  PW_CHECK(IsAlignedAs<size_t>(inner_alloc));
-  return *reinterpret_cast<Header*>(reinterpret_cast<uintptr_t>(inner_alloc) -
-                                    sizeof(Header));
-}
-
-template <typename Prefix>
-Prefix& FramedAllocation<Prefix>::GetPrefixFromInner(void* inner_alloc) {
-  Header& header = GetHeaderFromInner(inner_alloc);
-  return header.prefix;
-}
-
-template <typename Prefix>
-void FramedAllocation<Prefix>::DeallocateFromInner(Allocator* allocator,
-                                                   void* inner_alloc) {
-  PW_DCHECK(allocator != nullptr);
-  PW_DCHECK(inner_alloc != nullptr);
-
-  void* outer_alloc = nullptr;
-
-  {
-    Header& header = GetHeaderFromInner(inner_alloc);
-    outer_alloc = reinterpret_cast<void*>(
-        reinterpret_cast<uintptr_t>(inner_alloc) - header.offset);
-    std::destroy_at<Header>(&header);
-  }
-
-  allocator->Deallocate(outer_alloc);
-}
+// TODO: https://pwbug.dev/529845745 - Extract utility functions to use here
+// and with FramingAllocator to reduce similar/duplicate code. Just take care
+// that the functions do not increase the code size in any meaningful way.
 
 struct CoroAllocationHeader {
   Allocator* allocator;
-};
+  void* outer_alloc;
 
-using CoroFramedAllocation = FramedAllocation<CoroAllocationHeader>;
+  CoroAllocationHeader(Allocator* alloc, void* outer)
+      : allocator(alloc), outer_alloc(outer) {}
+};
 
 }  // namespace
 
@@ -166,22 +51,40 @@ void* CoroPromiseBase::SharedNew(CoroContext coro_cx,
   const auto coro_layout =
       allocator::Layout(size, static_cast<std::size_t>(align));
 
-  const auto framed_layout = CoroFramedAllocation::GetOuterLayout(coro_layout);
+  const size_t padding_size = coro_layout.alignment();
+  const auto outer_layout = allocator::Layout(
+      coro_layout.size() + sizeof(CoroAllocationHeader) + padding_size,
+      alignof(CoroAllocationHeader));
+
   PW_LOG_DEBUG("Allocating %zu B for %zu B coroutine with %zu B alignment",
-               framed_layout.size(),
+               outer_layout.size(),
                coro_layout.size(),
                static_cast<std::size_t>(align));
 
-  // Allocate space for the header and the opaque coroutine state.
   Allocator* const allocator = &coro_cx.allocator();
-  auto [header, coro_alloc] =
-      CoroFramedAllocation::Allocate(allocator, coro_layout);
-  if (coro_alloc == nullptr) {
+  auto* outer_alloc = allocator->Allocate(outer_layout);
+  if (outer_alloc == nullptr) {
     return nullptr;
   }
 
-  // Store the allocator in the header for use in deallocation.
-  header->allocator = allocator;
+  const uintptr_t outer_addr = reinterpret_cast<uintptr_t>(outer_alloc);
+
+  // We need coro_alloc to be aligned to coro_layout.alignment(), and
+  // coro_alloc >= outer_addr + sizeof(CoroAllocationHeader).
+  const uintptr_t min_coro_addr = outer_addr + sizeof(CoroAllocationHeader);
+  const uintptr_t coro_addr =
+      pw::AlignUp(min_coro_addr, coro_layout.alignment());
+
+  void* const coro_alloc = reinterpret_cast<void*>(coro_addr);
+
+  // Construct the header in the space between the outer allocation and the
+  // inner coroutine allocation, and store the allocator and pointer to the
+  // outer allocation there.
+  std::construct_at<CoroAllocationHeader>(
+      reinterpret_cast<CoroAllocationHeader*>(coro_addr -
+                                              sizeof(CoroAllocationHeader)),
+      allocator,
+      outer_alloc);
 
   // Return the pointer to the memory to use for the coroutine state.
   return coro_alloc;
@@ -199,9 +102,15 @@ void CoroPromiseBase::SharedDelete(
                size,
                static_cast<std::size_t>(align));
 
-  auto& header = CoroFramedAllocation::GetPrefixFromInner(ptr);
-  PW_DCHECK(header.allocator != nullptr);
-  CoroFramedAllocation::DeallocateFromInner(header.allocator, ptr);
+  auto* header = reinterpret_cast<CoroAllocationHeader*>(
+      reinterpret_cast<uintptr_t>(ptr) - sizeof(CoroAllocationHeader));
+
+  // Copy the values in the header so we can use them after destroying it.
+  Allocator* allocator = header->allocator;
+  void* outer_alloc = header->outer_alloc;
+
+  std::destroy_at<CoroAllocationHeader>(header);
+  allocator->Deallocate(outer_alloc);
 }
 
 }  // namespace pw::async2::internal
