0275a1b696fde99affb51443adce1e85f75f6462
Ewout van Bekkum <ewout@google.com>
2026-08-07 14:51:04 -0700

    pw_sync_zephyr: Assert successful k_mutex_lock return in Mutex::lock()
    Wrap k_mutex_lock in PW_DASSERT(k_mutex_lock(...) == 0) in
    Mutex::lock() to ensure lock acquisition errors are caught
    immediately during debug builds.
    
    TAG=agy
    CONV=042811d4-3b57-42cc-bbe5-b36a309f536a
    
    Change-Id: I5381d8efbd9896ecf8a34af70480c5e02fb3351e
    Reviewed-on: https://pigweed-review.googlesource.com/c/pigweed/pigweed/+/450612

--- pw_sync_zephyr/public/pw_sync_zephyr/mutex_inline.h
+++ pw_sync_zephyr/public/pw_sync_zephyr/mutex_inline.h
@@ -29,7 +29,7 @@ inline Mutex::~Mutex() = default;
 
 inline void Mutex::lock() {
   PW_DASSERT(!interrupt::InInterruptContext());
-  k_mutex_lock(&native_type_, K_FOREVER);
+  PW_DASSERT(k_mutex_lock(&native_type_, K_FOREVER) == 0);
   PW_DASSERT(native_type_.lock_count == 1);
 }
 
